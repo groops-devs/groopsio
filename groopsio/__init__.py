3ba3cebbb94c18ffb17c7b22662214ab437ed321
Andreas Kvas <kvas@tugraz.at>
2020-04-21 14:38:08 +0200

    initial commit

---
+++ groopsio/__init__.py
@@ -0,0 +1,12 @@
+"""
+GROOPS Python Package
+
+A collection of Python classes and functions to complement
+the functionality of GROOPS.
+
+io:
+    Read and write GROOPS files.
+
+"""
+
+__all__ = ['io']
