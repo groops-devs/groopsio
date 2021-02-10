fbbd73c47107b8e5e0f84403487bb6c73d494f72
Andreas Kvas <kvas@tugraz.at>
2021-02-10 11:36:14 +0100

    updated copyright notice

--- src/groopsio.cpp
+++ src/groopsio.cpp
@@ -1,7 +1,7 @@
 /*
  * Python module for GROOPS file I/O.
  *
- * Copyright (C) 2020 Andreas Kvas
+ * Copyright (C) 2020 - 2021 GROOPS Developers
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
